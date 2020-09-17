92c0af9562d678a56eeb2ccb1dc3a31b1c5576f6
High_King_Margo <69987194+highkingmargo@users.noreply.github.com>
2020-09-16 20:33:37 -0400

    Revert "Improved styling"
    This reverts commit 312b5c5312db13646b306cb0dbf58aababeff871.

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="#52575d"
-      size={60}
+      size={75}
       animate={true}
     />
   );
