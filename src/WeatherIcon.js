8d17cd5afdd0843199502a05a713ea91c5ea8270
High_King_Margo <69987194+highkingmargo@users.noreply.github.com>
2020-09-16 20:33:02 -0400

    Revert "Rolled back broken styling changes"
    This reverts commit 17889063833cc21a064f890c045e39fe63bcbf75.

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,8 +26,8 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="#696969"
-      size={75}
+      color="#52575d"
+      size={60}
       animate={true}
     />
   );
